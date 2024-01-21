c46a7fa33ce8ace988b789e3f8beca5a4f9ccc76
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2024-01-21 23:18:05 +0900

    feat: get postlist from posts folder

--- public/getpost.js
+++ public/getpost.js
@@ -9,12 +9,44 @@ const getPosts = async () => {
         if (err) {
             return console.log("Failed to list contents of directory: "+err)
         }
-        console.log(files)
         files.forEach((file, i) => {
             let obj = {}
             let post
             fs.readFile(`${dirPath}/${file}`, "utf-8", (err, contents) =>{
-
+                const getMetadataIndices = (acc, elem, i) => {
+                    if (/^---/.test(elem)) {
+                        acc.push(i)
+                    }
+                    return acc
+                }
+                const parseMetadata = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        let metadata = lines.slice(metadataIndices[0] + 1,
+                            metadataIndices[1])
+                            metadata.forEach(line => {
+                                obj[line.split(":")[0]] = line.split(":")[1].trim()
+                            })
+                            return obj
+                    }
+                }
+                const parseContent = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        lines = lines.slice(metadataIndices[1] + 1, lines.length)
+                    }
+                    return lines.join("\n")
+                }
+                const lines = contents.split("\n")
+                const metadataIndices = lines.reduce(getMetadataIndices, [])
+                const metadata = parseMetadata({lines, metadataIndices})
+                const content = parseContent({lines, metadataIndices})
+                post = {
+                    id: i + 1,
+                    title: metadata.title ? metadata.title : "No title given",
+                    author: metadata.author ? metadata.author : "No author",
+                    data: metadata.data ? metadata.data : "No data given",
+                    content: content ? content : "No content given"
+                }
+                postlist.push(post)
             })
         })
     })
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import AboutPage from "./_pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
+import ContactPage from "./_pages/ContactPage";
 import StartPage from "./_pages/start/StartPage";
 import PostPage from "./_pages/posts/PostsPage";
+
+const basename = process.env.PUBLIC_URL;
 
 export default function Router() {
   return (
     <BrowserRouter>
       <nav>
-          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/'><h1 class="title">이새힘 Iirin</h1></NavLink><br/>
-          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/about'>👀 About</NavLink><br/>
-          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/contact'>📭 Contact</NavLink><br/>
+          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/blog/'><h1 class="title">이새힘 Iirin</h1></NavLink><br/>
+          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/blog/about'>👀 About</NavLink><br/>
+          <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/blog/post'>📋 Posts</NavLink><br/>
       </nav>
 
       <Routes>
-        <Route path='/' element={<StartPage />} />
-        <Route path='/about' element={<AboutPage/>}/>
-        <Route path='/post' element={<PostPage/>}/>
+        <Route path='/blog/start' element={<StartPage />} />
+        <Route path='/blog/about' element={<AboutPage/>}/>
+        <Route path='/blog/post' element={<PostPage/>}/>
       </Routes>
     </BrowserRouter>
   );
